da237797901a8822c17166a3d1ad7e86fa37fc8b
Francis Mawn <francis.mawn@canada.ca>
2019-10-16 10:01:33 -0400

    Cleanup weather api code

--- plugins/craft-sapbot/src/services/Weather.php
+++ plugins/craft-sapbot/src/services/Weather.php
@@ -27,9 +27,9 @@ class Weather extends Component
 
     public function getWeatherFor($location, $language = 'en')
     {
-        $answer = 'I\'m sorry, I couldn\'t find your location. Please try again with a broader region.';
-
         try {
+            $answer = 'I\'m sorry, I couldn\'t find your location. Please try again with a broader region.';
+
             $response = $this->client->request('GET', "{$this->settings->weatherApiURL}/weather", [
                 'query' => [
                     'appid'    => $this->settings->weatherApiToken,
@@ -47,33 +47,16 @@ class Weather extends Component
                 return $answer;
             }
 
-            $weather = $body->weather[0]->main;
+            // Read weather from API response and return a human friendly message.
             $description = $body->weather[0]->description;
             $temperature = round($body->main->temp);
             $city = $location['city'];
-
-            /*
-            let mainWeather = body.weather[0].main;
-            if (mainWeather === 'Clear') {
-                mainWeather = 'Sun';
-            }
-            let mainDesc = body.weather[0].description;
-
-            // reset location memory
-            delete req.body.conversation.memory.location;
-
-            if (language == 'fr') {
-                replyText = `La prévision pour ${location.formatted} aujourd'hui est **${mainDesc.toLowerCase()}**, avec une température ${weatherAdjFR(body.main.temp)} de **${Math.round(body.main.temp)} °C**! (${Math.round(body.main.temp_max)}/${Math.round(body.main.temp_min)}) ${weatherEmoji(mainWeather)}`;
-            } else {
-                replyText = `The forecast for ${location.formatted} today is **${mainDesc.toLowerCase()}**, with a ${weatherAdjEN(body.main.temp)} temperature of **${Math.round(body.main.temp)}°C**! (${Math.round(body.main.temp_max)}/${Math.round(body.main.temp_min)}) ${weatherEmoji(mainWeather)}`;
-            }
-            */
             $answer = "The forecast for $city today is $description, with a temperature of {$temperature}°C!";
+
             return $answer;
-
-
         } catch (ClientException $e) {
-            return $answer;
+            // Something went wrong with the request, return default answer.
+            return 'I\'m sorry, I couldn\'t get the weather just now, maybe try later?';
         }
     }
 }
